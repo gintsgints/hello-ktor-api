8dd168855a3ef3e3ebd31affb6b04b4f38895acc
Gints Polis <gints.polis@lattelecom.lv>
2019-08-03 18:52:53 +0300

    Bin directory is wrong.

--- bin/main/entity/DatabaseConnection.kt
+++
@@ -1,6 +0,0 @@
-package eu.techwares.demo.entity
-
-import org.jetbrains.squash.connection.DatabaseConnection
-import org.jetbrains.squash.dialects.postgres.PgConnection
-
-fun db(): DatabaseConnection = PgConnection.create("localhost:5432/postgres", "postgres", "postgres_234")
